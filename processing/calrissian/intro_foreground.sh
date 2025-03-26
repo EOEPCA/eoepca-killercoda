854fd139a71fc6d74a813c5336788ed75ff10b75
Salvatore Pinto <3627896+spinto@users.noreply.github.com>
2025-03-26 12:05:44 +0100

    final intro

--- processing/calrissian/intro_foreground.sh
+++ processing/calrissian/intro_foreground.sh
@@ -1,1 +1,1 @@
-reset; tail -f /tmp/killercoda_setup.log; echo "setup complete! you can start teh tutorial now!"
+reset; tail -f /tmp/killercoda_setup.log; echo "setup complete! you can start the tutorial now!"
