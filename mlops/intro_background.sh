4ed64f9ddd68bc10d26ceac310efdf4242e41a7d
james-hinton <james.hinton@live.co.uk>
2025-06-05 13:19:49 +0000

    fix: remove KUBECONFIG export from custom Kubernetes aliases and ensure .bashrc is sourced

--- mlops/intro_background.sh
+++ mlops/intro_background.sh
@@ -42,6 +42,7 @@ fi
 
 helm plugin install https://github.com/aslafy-z/helm-git --version 1.3.0
 
-echo -e "\n# Custom Kubernetes Aliases\nexport KUBECONFIG=~/code/kubeconfig.yaml\nalias k=kubectl\nalias o=xdg-open\nalias ns=~/set-namespace.sh\nalias vcc=\"vcluster connect vcluster-deployment-test --namespace vcluster-deployment-test\"\nalias p=\"kubectl get pods\"\nalias lo=\"kubectl logs\"\nalias i=\"kubectl get ingress\"\nalias svc=\"kubectl get svc\"\nalias pvc=\"kubectl get pvc\"\nalias d=\"kubectl describe pod\"" >> ~/.bashrc
+echo -e "\n# Custom Kubernetes Aliases\nalias k=kubectl\nalias o=xdg-open\nalias ns=~/set-namespace.sh\nalias vcc=\"vcluster connect vcluster-deployment-test --namespace vcluster-deployment-test\"\nalias p=\"kubectl get pods\"\nalias lo=\"kubectl logs\"\nalias i=\"kubectl get ingress\"\nalias svc=\"kubectl get svc\"\nalias pvc=\"kubectl get pvc\"\nalias d=\"kubectl describe pod\"" >> ~/.bashrc
+source ~/.bashrc
 
 while ! killall tail; do sleep 1; done
