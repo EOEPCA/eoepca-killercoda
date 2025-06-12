963d150bbdaa786de50c5848a9a20fe999b491ed
Richard Conway <richard@rconway.co.uk>
2025-06-12 09:17:46 +0100

    [IAM] Wait for OPA gatekeeper to be ready

--- iam/intro_background.sh
+++ iam/intro_background.sh
@@ -23,6 +23,8 @@ if [[ -e /tmp/assets/ignoreresrequests ]]; then
   ### THIS IS JUST FOR DEMO! DO NOT DO THIS PART IN PRODUCTION!
   echo setting resource limits...  >> /tmp/killercoda_setup.log
   kubectl apply -f https://raw.githubusercontent.com/open-policy-agent/gatekeeper/v3.18.2/deploy/gatekeeper.yaml
+  echo ...waiting for gatekeeper readiness...  >> /tmp/killercoda_setup.log
+  kubectl rollout status gatekeeper-controller-manager -n gatekeeper-system
   cat <<EOF | kubectl apply -f -
 apiVersion: mutations.gatekeeper.sh/v1
 kind: Assign
